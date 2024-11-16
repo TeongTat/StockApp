95719785ac3c97ca17ec326eadc1ef8ab97aa9ed
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-11-16 15:29:11 +0800

    Update home.py

--- home.py
+++ home.py
@@ -5,7 +5,3 @@ st.set_page_config(layout="wide")
 # Main title and description for the app
 st.title("S&P 500 Stock Information & Prediction App")
 st.write("Welcome! Use the sidebar to navigate between pages to view stock information or set up predictions.")
-
-# Sidebar for navigation - acts as a tab selector
-st.sidebar.title("Navigation")
-selected_option = st.sidebar.radio("Go to:", ["Stock Information", "Stock Prediction"])
