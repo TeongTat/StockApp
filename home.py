7895ea78f89b374966b7f9945003e63aa651523a
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-11-17 09:04:56 +0800

    Update home.py

--- home.py
+++ home.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 #st.set_page_config(layout="wide")
 
+#set tittle page
 st.tittle("Stock Predictor App")
 
-st.write("""
-
+st.write(
+"""
 This is a stock price and prediction modelling app that will assist investors on buying or selling the stocks. The stocks are based on all companies listed on S&P 500 and the price are up to date linking from Yahoo Finance server.
 The app will display the following:
 - Historical stock price trend up to the latest.
 - Showcase stock price forecast (up to 10days).
 
 The main purpose of this application is providing price guidance for investors on the price risks and market risks of the S&P 500 stocks.
-
 """)
