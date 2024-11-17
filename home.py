64ec5f91956c4144dade360a8895f443222a6c6f
TeongTat <102958559+TeongTat@users.noreply.github.com>
2024-11-17 08:47:30 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,3 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
-
-# Main title and description for the app
-st.title("S&P 500 Stock Information & Prediction App")
-st.write("Welcome! Use the sidebar to navigate between pages to view stock information or set up predictions.")
